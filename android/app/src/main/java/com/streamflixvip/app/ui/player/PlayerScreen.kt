55f24da9170fdcfe9333964267d19376012acb83
StartStatic1 <startstatic@outlook.com>
2026-07-22 16:21:38 -0300

    Add files via upload

--- android/app/src/main/java/com/streamflixvip/app/ui/player/PlayerScreen.kt
+++ android/app/src/main/java/com/streamflixvip/app/ui/player/PlayerScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -42,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
