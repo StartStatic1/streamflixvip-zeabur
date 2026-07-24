62eeee9d9860a0376fe88b8aec977f0aa151c360
StartStatic1 <startstatic@outlook.com>
2026-07-23 22:01:35 -0300

    feat: app TV completo com Home moderna, Detalhe rico e Player nativo

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
