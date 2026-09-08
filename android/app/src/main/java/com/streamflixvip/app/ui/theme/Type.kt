cc5c8561b865300aab4ba16928aae585444ba2bb
StartStatic1 <2.7404228e+08+StartStatic1@users.noreply.github.com>
2026-09-08 11:41:22 +0000

    fix: adicionar certificados do Google Fonts

--- android/app/src/main/java/com/streamflixvip/app/ui/theme/Type.kt
+++ android/app/src/main/java/com/streamflixvip/app/ui/theme/Type.kt
@@ -7,12 +7,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.googlefonts.R as GoogleFontsR
+import com.streamflixvip.app.R
 
 private val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
-    certificates = GoogleFontsR.array.com_google_android_gms_fonts_certs,
+    certificates = R.array.com_google_android_gms_fonts_certs,
 )
 
 private val manrope = GoogleFont("Manrope")
