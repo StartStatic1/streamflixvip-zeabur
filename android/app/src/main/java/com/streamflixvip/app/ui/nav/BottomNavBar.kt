04795682550d219b7ae8fe3f401eedafae4af7dd
StartStatic1 <startstatic@outlook.com>
2026-09-09 16:20:15 -0300

    chore(nav): limpar imports nao usados na bottom bar

--- android/app/src/main/java/com/streamflixvip/app/ui/nav/BottomNavBar.kt
+++ android/app/src/main/java/com/streamflixvip/app/ui/nav/BottomNavBar.kt
@@ -3,7 +3,6 @@ package com.streamflixvip.app.ui.nav
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -22,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
